60dc32bcc361adcef4467fe32a8469b481c97fcc
David Rivas <david.rivas@pevaar.com>
2019-04-11 09:09:11 -0500

    ApiConstants added

---
+++ XamarinMarvelChallenge/XamarinMarvelChallenge/Constants/ApiConstants.cs
@@ -0,0 +1,15 @@
+﻿using PCLAppConfig;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinMarvelChallenge.Constants
+{
+    class ApiConstants
+    {
+        public readonly string ApiBaseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"];
+        public readonly string PublicKey = ConfigurationManager.AppSettings["PublicKey"];
+        public readonly string PrivateKey = ConfigurationManager.AppSettings["PrivateKey"];
+        public const string Attribution = "Data provided by Marvel. © 2014 Marvel";
+    }
+}
