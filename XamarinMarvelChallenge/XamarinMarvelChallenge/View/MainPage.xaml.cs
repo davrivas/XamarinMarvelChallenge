0cbb5de8f47f12ef66ac573fe5d5b555d8d05c95
David Rivas <david.rivas@pevaar.com>
2019-04-15 17:04:45 -0500

    MainPage View and ViewModel partially implemented

--- XamarinMarvelChallenge/XamarinMarvelChallenge/View/MainPage.xaml.cs
+++ XamarinMarvelChallenge/XamarinMarvelChallenge/View/MainPage.xaml.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XamarinMarvelChallenge.MarvelApi;
+using XamarinMarvelChallenge.ViewModel;
 
 namespace XamarinMarvelChallenge.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        private readonly MainPageViewModel _viewModel;
+        private readonly RestApi _restAPI;
+
         public MainPage()
         {
             InitializeComponent();
+            _viewModel = new MainPageViewModel();
+            _restAPI = new RestApi();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            _viewModel.Characters = await _restAPI.GetCharacters();
+
+            BindingContext = _viewModel;
         }
     }
 }
---
+++ XamarinMarvelChallenge/XamarinMarvelChallenge/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using XamarinMarvelChallenge.Model;
+
+namespace XamarinMarvelChallenge.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Character> Characters { get; set; }
+
+        public MainPageViewModel()
+        {
+
+        }
+    }
+}
