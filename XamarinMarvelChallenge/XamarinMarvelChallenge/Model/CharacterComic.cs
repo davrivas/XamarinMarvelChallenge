fc04391ce6bf5cc52af1a95cc8ada48f0e1336e6
David Rivas <david.rivas@pevaar.com>
2019-04-25 14:35:24 -0500

    Improved model

---
+++ XamarinMarvelChallenge/XamarinMarvelChallenge/Model/CharacterComic.cs
@@ -0,0 +1,8 @@
+﻿namespace XamarinMarvelChallenge.Model
+{
+    public class CharacterComic
+    {
+        public string ComicTitle { get; set; }
+        public string ComicResourceURI { get; set; }
+    }
+}
