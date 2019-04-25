d177925d2b01d33e2bb9d665d4683722fedff29b
David Rivas <david.rivas@pevaar.com>
2019-04-25 15:42:04 -0500

    Limit orientation only portrait

--- XamarinMarvelChallenge/XamarinMarvelChallenge.Android/MainActivity.cs
+++ XamarinMarvelChallenge/XamarinMarvelChallenge.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace XamarinMarvelChallenge.Droid
 {
-    [Activity(Label = "XamarinMarvelChallenge", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Xamarin Marvel Challenge", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
